b411fe327844449e1044bbb01a30257b27a56c70
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-05-20 13:24:00 +0100

    Passed test for even character

--- lib/middle_letter.rb
+++ lib/middle_letter.rb
@@ -4,9 +4,9 @@ class Middle_Letter
 
   def get_middle(character)
     if character.length.odd?
-      character.split('')[character.length / 2]
+      character.split('')[character.length/2]
     else
+      character.split('')[character.length/2 -1] + character.split('')[character.length / 2]  
     end
   end
-  
 end
--- spec/middle_letter_spec.rb
+++ spec/middle_letter_spec.rb
@@ -6,8 +6,10 @@ describe Middle_Letter do
   subject(:character) { described_class.new }
 
   describe '#get_middle' do
-    xit 'can find the middle character of word' do
-      expect(character.get_middle('test')).to eq 'es'
+    context 'even characters ' do 
+      it 'can find the middle character of word' do
+        expect(character.get_middle('test')).to eq 'es'
+      end
     end
 
     context 'odd characters' do
